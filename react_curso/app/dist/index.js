53542a4c985e2e437e13cfdf40d6274abf69962b
EAdeveloper <eddeveloper@hotmail.com>
2017-05-30 22:19:27 -0400

    First Component
    -JXS
    -Dependecies
    -Server

--- react_curso/app/dist/index.js
+++ react_curso/app/dist/index.js
@@ -1,2 +1,20 @@
-require('./modulo1.js');
-require('./modulo2.js');
+import React from "react";
+import ReactDom from "react-dom";
+
+class Layout extends React.Component {
+	constructor(){
+		super();
+		this.nombre = "Edward Almanzar";
+	}
+	obtenerNombre(){
+		return "Edward :)";
+	}
+	render() {
+		// return( <h1>Hola {this.nombre}</h1> );
+		return( <h1>Hola {this.obtenerNombre()}</h1> );
+	}
+}
+
+const app = document.getElementById('app');
+
+ReactDom.render(<Layout/>, app);
